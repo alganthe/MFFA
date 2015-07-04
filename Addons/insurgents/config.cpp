bb56c7aa876d581aebdf000b7768b67e1b2e6a07
Josuan Albin <alganthe@live.fr>
2015-07-04 11:50:03 +0200

    Indep insurgents added
    - Ported the whole opfor variant to indep.

--- Addons/insurgents/config.cpp
+++ Addons/insurgents/config.cpp
@@ -4,6 +4,7 @@ class cfgPatches {
     class ADDON {
         units[] = 
         {
+//////////////////////////// Opfor units
             "O_ti_CL_F",
             "O_ti_CL_u_F",
 			"O_ti_SL_F",
@@ -41,7 +42,46 @@ class cfgPatches {
             "O_ti_g_sup_eng",
             "O_ti_mot_g",
             "O_ti_mot_g_s",
-            "O_ti_mot_tech"
+            "O_ti_mot_tech",
+//////////////////////////// Indep units
+            "I_ti_CL_F",
+            "I_ti_CL_u_F",
+			"I_ti_SL_F",
+			"I_ti_soldier_F",
+			"I_ti_m_F",
+			"I_ti_gl_F",
+			"I_ti_medic_F",
+			"I_ti_light_F",
+			"I_ti_pistol_F",
+			"I_ti_lat_F",
+			"I_ti_aa_F",
+			"I_ti_mg_F",
+            "I_ti_ar_F",
+			"I_ti_aar_F",
+			"I_ti_eng_F",
+			"I_ti_exp_F",
+			"I_ti_driver_F",
+			"I_ti_technical_F",
+			"I_ti_technical_a_F",
+            "I_ti_technical_r_F",
+			"I_ti_truck_F",
+			"I_ti_truck_s_F",
+            "I_ti_suv_F",
+            "I_ti_fuel_F",
+            "I_ti_mortar_F",
+            "I_ti_g_team_F",
+            "I_ti_g_squad_F",
+            "I_ti_g_squad_s_F",
+            "I_ti_g_sentry_F",
+            "I_ti_g_sentry_r_F",
+            "I_ti_g_aa",
+            "I_ti_g_at",
+            "I_ti_g_sup_med",
+            "I_ti_g_sup_exp",
+            "I_ti_g_sup_eng",
+            "I_ti_mot_g",
+            "I_ti_mot_g_s",
+            "I_ti_mot_tech"
         };
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
@@ -65,6 +105,18 @@ class CfgGroups
             #include "groups\O_ti_mot.hpp"
         };
     };
+	class Indep
+	{
+		side=2;
+		name="independent";
+		class I_ti
+		{
+            name="Takistani insurgents";
+            #include "groups\I_ti_ins.hpp"
+            #include "groups\I_ti_sup.hpp"
+            #include "groups\I_ti_mot.hpp"
+        };
+    };
 };
 class cfgWeapons
 {
@@ -78,6 +130,14 @@ class CfgFactionClasses
         flag = "";
 		icon="";
 		side=0;
+		priority=2;
+	};
+    class I_ti
+	{
+		displayName="Takistani insurgents";
+        flag = "";
+		icon="";
+		side=2;
 		priority=2;
 	};
 };
@@ -99,13 +159,31 @@ class CfgVehicleClasses
 	{
 		displayName="Static";
 	};
+	class I_ti_ins
+	{
+		displayName="Insurgents";
+	};
+	class I_ti_mot
+	{
+		displayName="Motorized groups";
+	};
+	class I_ti_sup
+	{
+		displayName="Supports";
+	};
+	class I_ti_stat
+	{
+		displayName="Static";
+	};
 };
 class CfgVehicles
 {
     #include "equipment\ti_backpacks.hpp"
     #include "units\O_ti_units.hpp"
+    #include "units\I_ti_units.hpp"
 };
 class Extended_Init_Eventhandlers
 {
     #include "units\O_ti_EH.hpp"
+    #include "units\I_ti_EH.hpp"
 };
