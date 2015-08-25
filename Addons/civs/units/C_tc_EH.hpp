ecea74b5b9eb351cc078f434dc53a52dc925b6fa
alganthe <alganthe@live.fr>
2015-08-25 18:11:21 +0200

    takistani civ EH fixed

--- Addons/civs/units/C_tc_EH.hpp
+++ Addons/civs/units/C_tc_EH.hpp
@@ -1,16 +1,16 @@
     class C_tc_man_1
     {
-        MFFA_init = "";
+        MFFA_init = "[_this select 0, ['TRYK_H_pakol2',1]] call BIS_fnc_unitHeadgear";
     };
     class C_tc_man_2
     {
-        MFFA_init = "";
+        MFFA_init = "[_this select 0, ['TRYK_H_pakol2',1]] call BIS_fnc_unitHeadgear";
     };
     class C_tc_man_3
     {
-        MFFA_init = "";
+        MFFA_init = "[_this select 0, ['TRYK_H_pakol2',1]] call BIS_fnc_unitHeadgear";
     };
     class C_tc_man_4
     {
-        MFFA_init = "";
+        MFFA_init = "[_this select 0, ['TRYK_H_pakol2',1]] call BIS_fnc_unitHeadgear";
     };
