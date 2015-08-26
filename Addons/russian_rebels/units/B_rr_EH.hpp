337071504c08621d8f41a1fa8fadafa47fed269e
alganthe <alganthe@live.fr>
2015-08-26 09:28:09 +0200

    B_rr EH fixed

--- Addons/russian_rebels/units/B_rr_EH.hpp
+++ Addons/russian_rebels/units/B_rr_EH.hpp
@@ -1,52 +1,52 @@
     class B_rr_CL_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0, ['H_Beret_blk',1]] call BIS_fnc_unitHeadgear";
     };
     class B_rr_SL_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0, ['H_MilCap_gry',1]] call BIS_fnc_unitHeadgear";
     };
     class B_rr_soldier_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0,['TRYK_H_woolhat',0.1,'H_Bandanna_gry',0.1,'H_Bandanna_blu',0.1,'H_Bandanna_camo',0.1,'H_Watchcap_blk',0.1,'rhs_beanie_green',0.1,'H_Cap_blk',0.1,'H_Cap_oli',0.1,'TRYK_H_woolhat',0.1]] call BIS_fnc_unitHeadgear";
     };
     class B_rr_m_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0,['TRYK_H_woolhat',0.1,'H_Bandanna_gry',0.1,'H_Bandanna_blu',0.1,'H_Bandanna_camo',0.1,'H_Watchcap_blk',0.1,'rhs_beanie_green',0.1,'H_Cap_blk',0.1,'H_Cap_oli',0.1,'TRYK_H_woolhat',0.1]] call BIS_fnc_unitHeadgear";
     };
     class B_rr_gl_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0,['TRYK_H_woolhat',0.1,'H_Bandanna_gry',0.1,'H_Bandanna_blu',0.1,'H_Bandanna_camo',0.1,'H_Watchcap_blk',0.1,'rhs_beanie_green',0.1,'H_Cap_blk',0.1,'H_Cap_oli',0.1,'TRYK_H_woolhat',0.1]] call BIS_fnc_unitHeadgear";
     };
     class B_rr_medic_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0,['TRYK_H_woolhat',0.1,'H_Bandanna_gry',0.1,'H_Bandanna_blu',0.1,'H_Bandanna_camo',0.1,'H_Watchcap_blk',0.1,'rhs_beanie_green',0.1,'H_Cap_blk',0.1,'H_Cap_oli',0.1,'TRYK_H_woolhat',0.1]] call BIS_fnc_unitHeadgear";
     };
     class B_rr_light_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0,['TRYK_H_woolhat',0.1,'H_Bandanna_gry',0.1,'H_Bandanna_blu',0.1,'H_Bandanna_camo',0.1,'H_Watchcap_blk',0.1,'rhs_beanie_green',0.1,'H_Cap_blk',0.1,'H_Cap_oli',0.1,'TRYK_H_woolhat',0.1]] call BIS_fnc_unitHeadgear";
     };
     class B_rr_lat_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0,['TRYK_H_woolhat',0.1,'H_Bandanna_gry',0.1,'H_Bandanna_blu',0.1,'H_Bandanna_camo',0.1,'H_Watchcap_blk',0.1,'rhs_beanie_green',0.1,'H_Cap_blk',0.1,'H_Cap_oli',0.1,'TRYK_H_woolhat',0.1]] call BIS_fnc_unitHeadgear";
     };
     class B_rr_aa_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0,['TRYK_H_woolhat',0.1,'H_Bandanna_gry',0.1,'H_Bandanna_blu',0.1,'H_Bandanna_camo',0.1,'H_Watchcap_blk',0.1,'rhs_beanie_green',0.1,'H_Cap_blk',0.1,'H_Cap_oli',0.1,'TRYK_H_woolhat',0.1]] call BIS_fnc_unitHeadgear";
     };
     class B_rr_mg_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0,['TRYK_H_woolhat',0.1,'H_Bandanna_gry',0.1,'H_Bandanna_blu',0.1,'H_Bandanna_camo',0.1,'H_Watchcap_blk',0.1,'rhs_beanie_green',0.1,'H_Cap_blk',0.1,'H_Cap_oli',0.1,'TRYK_H_woolhat',0.1]] call BIS_fnc_unitHeadgear";
     };
     class B_rr_amg_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0,['TRYK_H_woolhat',0.1,'H_Bandanna_gry',0.1,'H_Bandanna_blu',0.1,'H_Bandanna_camo',0.1,'H_Watchcap_blk',0.1,'rhs_beanie_green',0.1,'H_Cap_blk',0.1,'H_Cap_oli',0.1,'TRYK_H_woolhat',0.1]] call BIS_fnc_unitHeadgear";
     };
     class B_rr_eng_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0,['TRYK_H_woolhat',0.1,'H_Bandanna_gry',0.1,'H_Bandanna_blu',0.1,'H_Bandanna_camo',0.1,'H_Watchcap_blk',0.1,'rhs_beanie_green',0.1,'H_Cap_blk',0.1,'H_Cap_oli',0.1,'TRYK_H_woolhat',0.1]] call BIS_fnc_unitHeadgear";
     };
     class B_rr_exp_F
     {
-        MFFA_init = "(_this select 0) setVariable ['BIS_enableRandomization', true];";
+        MFFA_init = "[_this select 0,['TRYK_H_woolhat',0.1,'H_Bandanna_gry',0.1,'H_Bandanna_blu',0.1,'H_Bandanna_camo',0.1,'H_Watchcap_blk',0.1,'rhs_beanie_green',0.1,'H_Cap_blk',0.1,'H_Cap_oli',0.1,'TRYK_H_woolhat',0.1]] call BIS_fnc_unitHeadgear";
     };
