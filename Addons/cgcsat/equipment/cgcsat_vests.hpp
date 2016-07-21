8804fc9b872621d8d3d0fd0d2664e93d6f9c2f88
Josuan Albin <alganthe@live.fr>
2016-07-21 02:44:55 +0200

    Fix vests textures

--- Addons/cgcsat/equipment/cgcsat_vests.hpp
+++ Addons/cgcsat/equipment/cgcsat_vests.hpp
@@ -1,58 +1,103 @@
-    class V_HarnessO_brn;
+class Vest_NoCamo_Base;
+class V_HarnessO_brn: Vest_NoCamo_Base {
+    class itemInfo;
+};
 
-    class cg_HarnessO_cwp: V_HarnessO_brn {
-        DLC = "MFFA";
-        author= ECSTRING(main,author);
-        _generalMacro="cg_HarnessO_cwp";
-        displayName="LBV Harness (cwp)";
-        picture=PATHTOF(UI\cwpUI.paa);
-        hiddenSelectionsTextures[]= {
-            PATHTOF(data\cg_harness1_cwp.paa),
-            PATHTOF(data\cg_harness2_cwp.paa)
-        };
+class cg_HarnessO_cwp: V_HarnessO_brn {
+    DLC = "MFFA";
+    author= ECSTRING(main,author);
+    _generalMacro="cg_HarnessO_cwp";
+    scope=2;
+    displayName="LBV Harness (cwp)";
+    picture= QPATHTOF(UI\cwpUI.paa);
+    model="\A3\Characters_F\OPFOR\equip_o_vest01";
+    hiddenSelections[] = {"camo1","camo2"};
+    hiddenSelectionsTextures[]= {
+        QPATHTOF(data\cg_harness1_cwp.paa),
+        QPATHTOF(data\cg_harness2_cwp.paa)
+    };
+    class ItemInfo: ItemInfo {
+        uniformModel = "\A3\Characters_F\OPFOR\equip_o_vest01";
+        containerClass = "Supply160";
+        mass = 30;
+        hiddenSelections[] = {"Camo1","Camo2"};
+    };
+};
+
+ class cg_HarnessOGL_cwp: cg_HarnessO_cwp {
+    _generalMacro="cg_HarnessOGL_cwp";
+    displayName="LBV Grenadier Harness (cwp)";
+    model="\A3\Characters_F\OPFOR\equip_o_vest_gl";
+    class ItemInfo: ItemInfo {
+        uniformModel = "\A3\Characters_F\OPFOR\equip_o_vest01";
+        containerClass = "Supply120";
+        mass = 15;
+    };
+ };
+
+ class cg_HarnessO_cdp: V_HarnessO_brn {
+    DLC = "MFFA";
+    author= ECSTRING(main,author);
+    _generalMacro="cg_HarnessO_cdp";
+    scope=2;
+    displayName="LBV Harness (cdp)";
+    picture=PATHTOF(UI\cdpUI.paa);
+    model="\A3\Characters_F\OPFOR\equip_o_vest01";
+    hiddenSelections[] = {"camo1","camo2"};
+    hiddenSelectionsTextures[]= {
+        QPATHTOF(data\cg_harness1_cdp.paa),
+        QPATHTOF(data\cg_harness2_cdp.paa)
+    };
+    class ItemInfo: ItemInfo {
+        uniformModel = "\A3\Characters_F\OPFOR\equip_o_vest_gl";
+        containerClass = "Supply160";
+        mass = 30;
+        hiddenSelections[] = {"Camo1","Camo2"};
     };
 
-    class cg_HarnessOGL_cwp: cg_HarnessO_cwp {
-        DLC = "MFFA";
-        author= ECSTRING(main,author);
-        _generalMacro="cg_HarnessOGL_cwp";
-        displayName="LBV Grenadier Harness (cwp)";
-    };
+};
 
-    class cg_HarnessO_cdp: V_HarnessO_brn {
-        DLC = "MFFA";
-        author= ECSTRING(main,author);
-        _generalMacro="cg_HarnessO_cdp";
-        displayName="LBV Harness (cdp)";
-        picture=PATHTOF(UI\cdpUI.paa);
-        hiddenSelectionsTextures[]= {
-            PATHTOF(data\cg_harness1_cdp.paa),
-            PATHTOF(data\cg_harness2_cdp.paa)
-        };
+ class cg_HarnessOGL_cdp: cg_HarnessO_cdp {
+    _generalMacro="cg_HarnessOGL_cdp";
+    scope=2;
+    displayName="LBV Grenadier Harness (cdp)";
+    model="\A3\Characters_F\OPFOR\equip_o_vest_gl";
+    class ItemInfo: ItemInfo {
+        uniformModel = "\A3\Characters_F\OPFOR\equip_o_vest01";
+        containerClass = "Supply120";
+        mass = 15;
     };
+ };
 
-    class cg_HarnessOGL_cdp: cg_HarnessO_cdp {
-        DLC = "MFFA";
-        author= ECSTRING(main,author);
-        _generalMacro="cg_HarnessOGL_cdp";
-        displayName="LBV Grenadier Harness (cdp)";
+ class cg_HarnessO_cup: V_HarnessO_brn {
+    DLC = "MFFA";
+    author= ECSTRING(main,author);
+    _generalMacro="cg_HarnessO_cup";
+    scope=2;
+    displayName="LBV Harness (cup)";
+    picture= QPATHTOF(UI\cdpUI.paa);
+    model="\A3\Characters_F\OPFOR\equip_o_vest01";
+    hiddenSelections[] = {"camo1","camo2"};
+    hiddenSelectionsTextures[]= {
+        QPATHTOF(data\cg_harness1_cup.paa),
+        QPATHTOF(data\cg_harness2_cup.paa)
     };
+    class ItemInfo: ItemInfo {
+        uniformModel = "\A3\Characters_F\OPFOR\equip_o_vest01";
+        containerClass = "Supply160";
+        mass = 30;
+        hiddenSelections[] = {"Camo1","Camo2"};
+    };
+};
 
-    class cg_HarnessO_cup: V_HarnessO_brn {
-        DLC = "MFFA";
-        author= ECSTRING(main,author);
-        _generalMacro="cg_HarnessO_cup";
-        displayName="LBV Harness (cup)";
-        picture=PATHTOF(UI\cdpUI.paa);
-        hiddenSelectionsTextures[]= {
-            PATHTOF(data\cg_harness1_cup.paa),
-            PATHTOF(data\cg_harness2_cup.paa)
-        };
+ class cg_HarnessOGL_cup: cg_HarnessO_cup {
+    _generalMacro="cg_HarnessOGL_cup";
+    scope=2;
+    displayName="LBV Grenadier Harness (cup)";
+    model="\A3\Characters_F\OPFOR\equip_o_vest_gl";
+    class ItemInfo: ItemInfo {
+        uniformModel = "\A3\Characters_F\OPFOR\equip_o_vest01";
+        containerClass = "Supply120";
+        mass = 15;
     };
-
-    class cg_HarnessOGL_cup: cg_HarnessO_cup {
-        DLC = "MFFA";
-        author= ECSTRING(main,author);
-        _generalMacro="cg_HarnessOGL_cup";
-        displayName="LBV Grenadier Harness (cup)";
-    };
+};
